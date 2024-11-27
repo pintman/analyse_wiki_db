33fe493d1ea134ad5107e8f76a5fe171590f3503
Marco Bakera <bakera@tbs1.de>
2024-11-27 14:44:45 +0100

    cleanup

--- pagerank_iterativ2.py
+++ pagerank_iterativ2.py
@@ -103,31 +103,10 @@ def wikipages(user:str, password:str, database:str, pagetable:str, pagelinkstabl
     conn.close()
     return list(pages.values())
 
-def save_dot(pages, filename):
-    'Save pages to dotfile that can be rendered by graphviz: dot -o a.pdf -T pdf a.dot'
-    with open(filename, 'wt') as f:
-        f.write('digraph G{\n')
-        for p in pages:
-            for pout in p.outgoing_links:
-                num_p_outs = len(p.outgoing_links)
-                num_pout_outs = len(pout.outgoing_links)
-                f.write(f'"{p.name} #{num_p_outs}\\n{round(p.pr,3)}" -> "{pout.name} #{num_pout_outs}\\n{round(pout.pr,3)}";\n')
-                #f.write(f'"{p.name}" -> "{pout.name}";\n')
-
-        f.write('}\n')
-
-def print_pages(pagelist):
-    for p in pagelist:
-        print(p.name, "->", [pnach.name for pnach in p.outgoing_links])
 
 def main():
-    #ps = wikipages("root", "", "dewikiquote", "page", "pagelinks")
-    #ps = wikipages("root", "", "wikiitf14a", "wikipage", "wikipagelinks")
-    #ps = wikipages("root", "", "wikiitf17a", "page", "pagelinks")
-    #ps = wikipages("root", "", "wiki_itf18a", "page", "pagelinks")
     ps = wikipages("", "", "wiki_ITF19a.sqlite", "page", "pagelinks")
     print("read pages", len(ps))
-    print_pages(ps)
     calc_pr(ps, d=0.85, iterations=15, debug=False)
     ps.sort(reverse=True)
     print("RESULT\n", 
@@ -136,29 +115,8 @@ def main():
         [(p.name, round(p.pr, 3)) for p in ps[-3:]])
     print("SUM-PR", sum([p.pr for p in ps]))  
 
-    save_dot(ps, 'itf19a.dot')
-
-    """
-    ITF19a:
-    read pages 39
-    [('Mause', 3.806), ('Maus', 3.385), ('Bildschirm', 3.381), ('Hardware', 1.186), ('Software', 1.1)] 
-    ...
-    [('Schreiber', 0.15), ('Localhost', 0.15), ('Flowchart.png', 0.15)]
-    SUM-PR 21.94321975734223
-    """
-
     return ps
 
 
 if __name__ == "__main__":
     ps = main()
-
-    #import cProfile
-    #import pstats
-    #cProfile.run("main()", sort=pstats.SortKey.TIME)
-    # An welcher Stelle wird am meisten Zeit verbraucht? - fetch_row
-    # Zeilen werden einzeln aus der DB geholt. -> cursor.fetchall() verwendet
-    # 
-    # Running Profiler and saving statistics. Can be viewed with
-    # python -m pstats pagerank.pstats
-    #cProfile.run("main()", filename="pagerank.pstats")
